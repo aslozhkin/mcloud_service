fa69ded3a5f862e12f41379e685eaeed33b67a8d
aslozhkin <h#o345%$Ty*14O@q>
2021-02-20 13:25:45 +0300

    first commit

---
+++ src/main/java/ru/lanit/at/mcloud_service/McloudServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.lanit.at.mcloud_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McloudServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lanit/at/mcloud_service/McloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lanit.at.mcloud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
